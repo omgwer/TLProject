43fc64496ac0777ed36bc3fa8996b9448ebb7961
omgwer <63747048+omgwer@users.noreply.github.com>
2022-03-23 23:07:55 +0300

    init commit

---
+++ Ellipse/ConsoleApp/Ellipse.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp;
+public class Ellipse
+{
+    private int
+        horisontalRadius,
+        verticalRadius;
+     
+    private const int ROUND_ACCURACY = 2;
+    
+    public Ellipse(int verticalRadius, int horisontalRadius)
+    {
+        if (verticalRadius <= 0 | horisontalRadius <= 0)
+        {
+            throw new ArgumentException("Error with init object, arguments invalid");
+        }
+
+        this.verticalRadius = verticalRadius;
+        this.horisontalRadius = horisontalRadius;
+    }
+
+    public double getSquareEllipse()
+    {
+        return Math.Round(Math.PI * horisontalRadius * verticalRadius, ROUND_ACCURACY);
+    }
+
+    public double getСircumferenceEllipse()
+    {
+        return Math.Round(Math.PI * (horisontalRadius + verticalRadius), ROUND_ACCURACY);
+    }
+}
---
+++ Ellipse/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int verticalRadius = 5;
+            int horisontalRadius = 3;
+
+            try
+            {
+                Ellipse newEllipse = new Ellipse(verticalRadius, horisontalRadius);
+                Console.WriteLine("Ellipse Square is : " + newEllipse.getSquareEllipse());
+                Console.WriteLine("Ellipse circumference is : " + newEllipse.getСircumferenceEllipse());
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
