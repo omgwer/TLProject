5bfe29c05d721ebc16e01a8bdd131a53588cd99c
omgwer <63747048+omgwer@users.noreply.github.com>
2022-03-27 19:41:49 +0300

    Update ComputerFactory.cs

--- Computer/Computer/Components/Factory/ComputerFactory.cs
+++ Computer/Computer/Components/Factory/ComputerFactory.cs
@@ -1,5 +1,4 @@
 ﻿using Computer.Components.Builders;
-using Computer.Components.Helper;
 
 namespace Computer.Components.Factory;
 
@@ -8,7 +7,7 @@ public class ComputerFactory
     public Dictionary<string, Dictionary<string, string>> BuildDefaultPc()
     {
         var buildingPc = new ComputerBuilder();
-        
+
         var myNewProcessor = new ProcessorBuilder("DefaultProcessor");
         myNewProcessor.SetIntegratedVideo(true);
         myNewProcessor.SetProcessorSocket("Amd");
@@ -16,34 +15,33 @@ public class ComputerFactory
         myNewProcessor.SetProcessorThreadCount(2);
         myNewProcessor.SetProcessorThreadFrequency(2400);
         var processor = myNewProcessor.Build();
-            
+
         var myNewMotherBoard = new MotherboardBuilder("Motherboard");
         myNewMotherBoard.SetMotherboardSize("ATX");
         myNewMotherBoard.SetMotherboardSocket("Amd");
         myNewMotherBoard.SetMotherBoardRamSlotCount(4);
         var motherboard = myNewMotherBoard.Build();
-        
+
         var myNewRam = new RamBuilder("MegaRam");
         myNewRam.SetRamMemory(4000);
         myNewRam.SetRamStickCount(2);
         var ram = myNewRam.Build();
-        
+
         var myNewRom = new RomBuilder("MegaRom");
         myNewRom.SetRomMemory(500000);
         myNewRom.SetRomType("ssd");
         var rom = myNewRom.Build();
-        
+
         var myNewSustemUnit = new SystemUnitBuilder("Box");
         myNewSustemUnit.SetSystemUnitColor("red");
         myNewSustemUnit.SetSystemUnitSize("ATX");
         var sustemUnit = myNewSustemUnit.Build();
-        
+
         var myNewVideoCard = new VideoCardBuilder("MiningCard");
         myNewVideoCard.SetVideoCardProcessor("GeForce");
         myNewVideoCard.SetVideoCardMemorySize(250);
         var videoCard = myNewVideoCard.Build();
-            
-            
+
         buildingPc.AddComponent(processor);
         buildingPc.AddComponent(motherboard);
         buildingPc.AddComponent(ram);
